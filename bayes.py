a0755124da83dae6e93b6c8cca29cb5a8481a63f
David <oconned6@tcd.ie>
2022-06-11 17:53:35 +0100

    black formatted

--- bayes.py
+++ bayes.py
@@ -4,34 +4,37 @@ import itertools
 import numpy as np
 import cv2 as cv
 
-MAP_FILE = 'cape_python.png'
+MAP_FILE = "cape_python.png"
 
 SA1_CORNERS = (130, 265, 180, 315)  # (UL-X, UL-Y, LR-X, LR-Y)
-SA2_CORNERS = (80, 255, 130, 305)   # (UL-X, UL-Y, LR-X, LR-Y)
+SA2_CORNERS = (80, 255, 130, 305)  # (UL-X, UL-Y, LR-X, LR-Y)
 SA3_CORNERS = (105, 205, 155, 255)  # (UL-X, UL-Y, LR-X, LR-Y)
 
-class Search():
+
+class Search:
     """Bayesian Search & Rescue game with 3 search area."""
 
     def __init__(self, name):
         self.name = name
         self.img = cv.imread(MAP_FILE, cv.IMREAD_COLOR)
         if self.img is None:
-            print('Could not load map file {}'.format(MAP_FILE),
-                file=sys.stderr)
+            print("Could not load map file {}".format(MAP_FILE), file=sys.stderr)
             sys.exit(1)
 
         self.area_actual = 0
-        self.sailor_actual = [0,0] # As "local" coord within search area
-        
-        self.sa1 = self.img[SA1_CORNERS[1] : SA1_CORNERS[3],
-                            SA1_CORNERS[0] : SA1_CORNERS[2]]
+        self.sailor_actual = [0, 0]  # As "local" coord within search area
 
-        self.sa2 = self.img[SA2_CORNERS[1] : SA2_CORNERS[3],
-                            SA2_CORNERS[0] : SA2_CORNERS[2]]
+        self.sa1 = self.img[
+            SA1_CORNERS[1] : SA1_CORNERS[3], SA1_CORNERS[0] : SA1_CORNERS[2]
+        ]
 
-        self.sa3 = self.img[SA3_CORNERS[1] : SA3_CORNERS[3],
-                            SA3_CORNERS[0] : SA3_CORNERS[2]]
+        self.sa2 = self.img[
+            SA2_CORNERS[1] : SA2_CORNERS[3], SA2_CORNERS[0] : SA2_CORNERS[2]
+        ]
+
+        self.sa3 = self.img[
+            SA3_CORNERS[1] : SA3_CORNERS[3], SA3_CORNERS[0] : SA3_CORNERS[2]
+        ]
 
         self.p1 = 0.2
         self.p2 = 0.5
@@ -39,45 +42,91 @@ class Search():
 
         self.sep1 = 0
         self.sep2 = 0
-        self.sep3 = 0 
+        self.sep3 = 0
 
     def draw_map(self, last_known):
         """Display basemap with scale, last known xy location, searach areas."""
-        
-        #Draw a scale bar.
-        cv.line(self.img, (20,370), (70, 370), (0,0,0), 2)
-        cv.putText(self.img, '0', (8, 370), cv.FONT_HERSHEY_PLAIN, 1, (0,0,0))
-        cv.putText(self.img, '50 Nautical Miles', (71, 370),
-                    cv.FONT_HERSHEY_PLAIN, 1, (0,0,0))
 
+        # Draw a scale bar.
+        cv.line(self.img, (20, 370), (70, 370), (0, 0, 0), 2)
+        cv.putText(self.img, "0", (8, 370), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 0))
+        cv.putText(
+            self.img,
+            "50 Nautical Miles",
+            (71, 370),
+            cv.FONT_HERSHEY_PLAIN,
+            1,
+            (0, 0, 0),
+        )
 
         # Draw and nnumber the search areas.
-        cv.rectangle(self.img, (SA1_CORNERS[0], SA1_CORNERS[1]),
-                     (SA1_CORNERS[2], SA1_CORNERS[3]), (0, 0, 0), 1)
-        cv.putText(self.img, '1',
-                   (SA1_CORNERS[0] + 3, SA1_CORNERS[1] + 15),
-                   cv.FONT_HERSHEY_PLAIN, 1, 0)
-        cv.rectangle(self.img, (SA2_CORNERS[0], SA2_CORNERS[1]),
-                     (SA2_CORNERS[2], SA2_CORNERS[3]), (0, 0, 0), 1)
-        cv.putText(self.img, '2',
-                   (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15),
-                   cv.FONT_HERSHEY_PLAIN, 1, 0)
-        cv.rectangle(self.img, (SA3_CORNERS[0], SA3_CORNERS[1]),
-                     (SA3_CORNERS[2], SA3_CORNERS[3]), (0, 0, 0), 1)
-        cv.putText(self.img, '3',
-                   (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15),
-                   cv.FONT_HERSHEY_PLAIN, 1, 0)
+        cv.rectangle(
+            self.img,
+            (SA1_CORNERS[0], SA1_CORNERS[1]),
+            (SA1_CORNERS[2], SA1_CORNERS[3]),
+            (0, 0, 0),
+            1,
+        )
+        cv.putText(
+            self.img,
+            "1",
+            (SA1_CORNERS[0] + 3, SA1_CORNERS[1] + 15),
+            cv.FONT_HERSHEY_PLAIN,
+            1,
+            0,
+        )
+        cv.rectangle(
+            self.img,
+            (SA2_CORNERS[0], SA2_CORNERS[1]),
+            (SA2_CORNERS[2], SA2_CORNERS[3]),
+            (0, 0, 0),
+            1,
+        )
+        cv.putText(
+            self.img,
+            "2",
+            (SA2_CORNERS[0] + 3, SA2_CORNERS[1] + 15),
+            cv.FONT_HERSHEY_PLAIN,
+            1,
+            0,
+        )
+        cv.rectangle(
+            self.img,
+            (SA3_CORNERS[0], SA3_CORNERS[1]),
+            (SA3_CORNERS[2], SA3_CORNERS[3]),
+            (0, 0, 0),
+            1,
+        )
+        cv.putText(
+            self.img,
+            "3",
+            (SA3_CORNERS[0] + 3, SA3_CORNERS[1] + 15),
+            cv.FONT_HERSHEY_PLAIN,
+            1,
+            0,
+        )
 
         # Post the last known position of the sailor.
-        cv.putText(self.img, '+', (last_known), 
-                    cv.FONT_HERSHEY_PLAIN, 1, (0,0,255))
-        cv.putText(self.img, '+ = Last Known Position', (274, 355),
-                   cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
-        cv.putText(self.img, '* = Actual Position', (275, 370),
-                   cv.FONT_HERSHEY_PLAIN, 1, (255, 0, 0))
+        cv.putText(self.img, "+", (last_known), cv.FONT_HERSHEY_PLAIN, 1, (0, 0, 255))
+        cv.putText(
+            self.img,
+            "+ = Last Known Position",
+            (274, 355),
+            cv.FONT_HERSHEY_PLAIN,
+            1,
+            (0, 0, 255),
+        )
+        cv.putText(
+            self.img,
+            "* = Actual Position",
+            (275, 370),
+            cv.FONT_HERSHEY_PLAIN,
+            1,
+            (255, 0, 0),
+        )
 
-        cv.imshow('Search Area', self.img)
-        cv.moveWindow('Search Area', 750, 10)
+        cv.imshow("Search Area", self.img)
+        cv.moveWindow("Search Area", 750, 10)
         cv.waitKey(500)
 
     def sailor_final_location(self, num_search_areas):
@@ -88,7 +137,7 @@ class Search():
 
         area = int(random.triangular(1, num_search_areas + 1))
 
-        if area ==1:
+        if area == 1:
             x = self.sailor_actual[0] + SA1_CORNERS[0]
             y = self.sailor_actual[1] + SA1_CORNERS[1]
             self.area_actual = 1
@@ -99,15 +148,14 @@ class Search():
             x = self.sailor_actual[0] + SA3_CORNERS[0]
             y = self.sailor_actual[1] + SA3_CORNERS[1]
 
-        return x,y
-
+        return x, y
 
     def calc_search_effectiveness(self):
         """Set decimal search effectiveness value per search area."""
         self.sep1 = random.uniform(0.2, 0.9)
         self.sep2 = random.uniform(0.2, 0.9)
         self.sep3 = random.uniform(0.2, 0.9)
-        
+
     def conduct_search(self, area_num, area_array, effectiveness_prob):
         """Return search resutls and list of searched coordinates."""
 
@@ -115,24 +163,27 @@ class Search():
         local_x_range = range(area_array.shape[1])
         coords = list(itertools.product(local_x_range, local_y_range))
         random.shuffle(coords)
-        coords = coords[:int((len(coords) * effectiveness_prob))]
+        coords = coords[: int((len(coords) * effectiveness_prob))]
         loc_actual = (self.sailor_actual[0], self.sailor_actual[1])
         if area_num == self.area_actual[0] and loc_actual in coords:
-            return 'Found in Area {}.'.format(area_num), coords
+            return "Found in Area {}.".format(area_num), coords
         else:
-            return 'Not Found', coords
+            return "Not Found", coords
 
     def revise_target_probs(self):
         """Update area target probabilities based on search effectiveness."""
-        denom = self.p1 * (1-self.sep1) + self.p2 * (1 - self.sep2) \
+        denom = (
+            self.p1 * (1 - self.sep1)
+            + self.p2 * (1 - self.sep2)
             + self.p3 * (1 - self.sep3)
+        )
         self.p1 = self.p1 * (1 - self.sep1) / denom
         self.p2 = self.p2 * (1 - self.sep2) / denom
         self.p3 = self.p3 * (1 - self.sep3) / denom
 
     def draw_menu(search_num):
         """Print menu of choices for conducting area searches."""
-        print('\nSearch {}'.format(search_num))
+        print("\nSearch {}".format(search_num))
         print(
             """
             Choose next areas to search:
@@ -145,8 +196,8 @@ class Search():
             6 - Search Areas 2 & 3
             7 - Start Over
             """
-            )
+        )
 
     def main():
-        app = Search('Cape_Python')
+        app = Search("Cape_Python")
         app.draw_map(last_known=(160, 290))
