3381c50d15be0a07015baeb66336e0023cc9955e
David <oconned6@tcd.ie>
2022-06-13 12:43:11 +0100

    added remaining main code

--- bayes.py
+++ bayes.py
@@ -201,3 +201,85 @@ class Search:
     def main():
         app = Search("Cape_Python")
         app.draw_map(last_known=(160, 290))
+        sailor_x, sailor_y = app.sailor_final_location(num_search_areas=3)
+        print("-" * 65)
+        print("\nInitial Target (P) Probabilities:")
+        print("P1 = {:.3f}, P2 = {:.3f}, P3 = {:.3f}".format(app.p1, app.p2, app.p3))
+        search_num = 1
+
+        while True:
+            app.calc_search_effectiveness()
+            draw_menu(search_num)
+            choice = input("Choice: ")
+
+            if choice == "0":
+                sys.exit()
+
+            elif choice == "1":
+                results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
+                results_2, coords_2 = app.conduct_search(1, app.sa1, app.sep1)
+                app.sep1 = (len(set(coords_1 + coords_2))) / (len(app.sa1)**2)
+                app.sep2 = 0
+                app.sep3 = 0
+
+            elif choice == "2":
+                results_1, coords_1 = app.conduct_search(2, app.sa2, app.sep2)
+                results_2, coords_2 = app.conduct_search(2, app.sa2, app.sep2)
+                app.sep1 = 0
+                app.sep2 = (len(set(coords_1 + coords_2))) / (len(app.sa2)**2)
+                app.sep3 = 0
+
+            elif choice == "3":
+                results_1, coords_1 = app.conduct_search(3, app.sa3, app.sep3)
+                results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
+                app.sep1 = 0
+                app.sep2 = 0
+                app.sep3 = (len(set(coords_1 + coords_2))) / (len(app.sa3)**2)
+
+            elif choice == "4":
+                results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
+                results_2, coords_2 = app.conduct_search(2, app.sa2, app.sep2)
+                app.sep3 = 0
+
+            elif choice == "5":
+                results_1, coords_1 = app.conduct_search(1, app.sa1, app.sep1)
+                results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
+                app.sep2 = 0
+
+            elif choice == "6":
+                results_1, coords_1 = app.conduct_search(2, app.sa2, app.sep2)
+                results_2, coords_2 = app.conduct_search(3, app.sa3, app.sep3)
+                app.sep1 = 0
+
+            elif choice == "7":
+                main()
+
+            else:
+                print("\nSorry, but that isn't a valid choice.", file=sys.stderr)
+                continue
+
+            app.revise_target_probs()  # Use Bayes' rule to update target probs.
+
+            print("\nSearch {} Results 1 = {}"
+                .format(search_num, results_1), file=sys.stderr)
+            print("Search {} Results 2 = {}\n"
+                .format(search_num, results_2), file=sys.stderr)
+            print("Search {} Effectiveness (E):".format(search_num))
+            print("E1 = {:.3f}, E2 = {:.3f}, E3 = {:.3f}"
+                .format(app.sep1, app.sep2, app.sep3))
+
+            # Print target probabilities if sailor is not found else show position.
+            if results_1 == 'Not Found' and results_2 == 'Not Found':
+                print("\nNew Target Probabilities (P) for Search {}:"
+                    .format(search_num + 1))
+                print("P1 = {:.3f}, P2 = {:.3f}, P3 = {:.3f}"
+                    .format(app.p1, app.p2, app.p3))
+            else:
+                cv.circle(app.img, (sailor_x, sailor_y), 3, (255, 0, 0), -1)
+                cv.imshow('Search Area', app.img)
+                cv.waitKey(1500)
+                main()
+            search_num += 1
+
+if __name__ == '__main__':
+    main()
